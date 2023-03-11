200d4d2dab08959949e5863f1ec62928cba6a531
abhinabadutta2019 <abhinabadutta2019@gmail.com>
2023-03-11 23:38:43 +0530

    student details page

--- routes/studentRoutes.js
+++ routes/studentRoutes.js
@@ -4,11 +4,26 @@ const Task = require("../model/task");
 const router = express.Router();
 
 //Routers
+//"/students"
 //get route
 router.get("/", async (req, res) => {
   const tasks = await Task.find({});
   const students = await Student.find({});
   res.render("index.ejs", { tasks: tasks, students: students });
+});
+
+//get single page
+
+router.get("/single/:id", async (req, res) => {
+  try {
+    const student = await Student.findById(req.params.id);
+    // console.log(req.params.id);
+    // console.log(student);
+    // res.send();
+    res.render("studentDetails.ejs", { student: student });
+  } catch (e) {
+    console.log(e);
+  }
 });
 
 //seed route
