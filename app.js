f4d0efe34f112d724838c13164bbcf6286a0e4e1
abhinabadutta2019 <abhinabadutta2019@gmail.com>
2023-05-26 20:50:04 +0530

    updated database connection code to mongoDb cloud--( previously- local mongoDb )

--- app.js
+++ app.js
@@ -3,27 +3,39 @@ const mongoose = require("mongoose");
 const studentRoutes = require("./routes/studentRoutes");
 const taskRoutes = require("./routes/taskRoutes");
 const bodyParser = require("body-parser");
+const dotenv = require("dotenv");
 
 // express app
 const app = express();
-
-//Establishing mongoose
-const DATABASE_URL = "mongodb://127.0.0.1:27017/10th-March";
-const CONFIG = {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-};
-// // Establish Connection
-mongoose.connect(DATABASE_URL, CONFIG);
+dotenv.config();
 
 // register view engine
 app.set("view engine", "ejs");
 
-mongoose.connection
-  .on("open", () => console.log("Connected to Mongoose"))
-  .on("close", () => console.log("Disconnected from Mongoose"));
+//mongoDB cloud
+let uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.te788iv.mongodb.net/studentTaskEJS-26-may?retryWrites=true&w=majority`;
+//
+mongoose.connect(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
-// Middleware
+// //Establishing mongoose
+///////////////////////////////////////////////////////////////////
+// const DATABASE_URL = "mongodb://127.0.0.1:27017/10th-March";
+// const CONFIG = {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// };
+// // // Establish Connection
+// mongoose.connect(DATABASE_URL, CONFIG);
+
+// mongoose.connection
+//   .on("open", () => console.log("Connected to Mongoose"))
+//   .on("close", () => console.log("Disconnected from Mongoose"));
+//////////////////////////////////////////////////////////////////////
+
+//.. Middleware
 // app.use(morgan("tiny")); //logging
 app.use(express.urlencoded({ extended: true })); // parse urlencoded request bodies
 app.use("/static", express.static("static"));
